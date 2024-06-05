892578285adf5e756c137c1e7e2a2932949f14d4
MashaMy <masha_myh@mail.ru>
2024-06-05 12:46:22 +0300

    your message

--- src/Components/Filters/AllCategories.js
+++ src/Components/Filters/AllCategories.js
@@ -5,7 +5,7 @@ const AllCategories = () => {
         <h1 className="center">ШАРЫ И ИГРУШКИ</h1> 
 
         {['ШАРЫ НА ПОТОЛОК', 'ШАРЫ НАПОЛЬНЫЕ', 'ШАР ИЗ КОРОБКИ', 'ИГРУШКА 100 см', 'ПАРНЫЕ ИГРУШКИ', 'МЯГКИЕ ЗВЕРИ', 'ВСЕ ТОВАРЫ']
-        .map(category => <Filters key = {category.id} category = {category}/>
+        .map(category => <Filters key = {category} category = {category}/>
         )}
         
         </div>)
