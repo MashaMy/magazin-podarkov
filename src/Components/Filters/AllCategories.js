088f3deafa49238aed94035579c3e8da63807ceb
MashaMy <masha_myh@mail.ru>
2024-06-05 11:31:24 +0300

    your message

--- src/Components/Filters/AllCategories.js
+++ src/Components/Filters/AllCategories.js
@@ -4,7 +4,8 @@ const AllCategories = () => {
     return (<div>
         <h1 className="center">ШАРЫ И ИГРУШКИ</h1> 
 
-        {['ШАРЫ НА ПОТОЛОК', 'ШАРЫ НАПОЛЬНЫЕ', 'ШАР ИЗ КОРОБКИ', 'ИГРУШКА 100 см', 'ПАРНЫЕ ИГРУШКИ', 'МЯГКИЕ ЗВЕРИ', 'ВСЕ ТОВАРЫ'].map(category => <Filters key={category.id} category = {category}/>
+        {['ШАРЫ НА ПОТОЛОК', 'ШАРЫ НАПОЛЬНЫЕ', 'ШАР ИЗ КОРОБКИ', 'ИГРУШКА 100 см', 'ПАРНЫЕ ИГРУШКИ', 'МЯГКИЕ ЗВЕРИ', 'ВСЕ ТОВАРЫ']
+        .map(category => <Filters key = {category.id} category = {category}/>
         )}
         
         </div>)
